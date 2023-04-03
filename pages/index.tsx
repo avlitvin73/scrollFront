7110974261c482fcf0705d773f25f1580211fad4
Aleksander Litin <78950640+avlitvin73@users.noreply.github.com>
2023-04-04 02:35:20 +0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,34 +5,37 @@ import styles from '../styles/Home.module.css'
 import bg from '../public/img/ground.webp'
 import { useState, useEffect, useRef } from "react";
 import axios from 'axios';
+import {emoji} from './emoji'
 
-function encryptData(data, key) {
-  let encrypted = "";
-  for (let i = 0; i < data.length; i++) {
-    encrypted += String.fromCharCode(data.charCodeAt(i) ^ key);
-  }
-  return encrypted;
-}
 
 const joinClasses = (...classes: string[]) => {
   return classes.map((className) => styles[className]).join(" ");
 };
 
+const encrypt = (text, key) => {
+  let result = '';
+  for (let i = 0; i < text.length; i++) {
+    let char = text.charCodeAt(i);
+    let keychar = key.charCodeAt(i % key.length);
+    result += String.fromCharCode(char ^ keychar);
+  }
+  return result;
+}
+
 const Home: NextPage = () => {
-  
   const [isModalOpen, setIsModalOpen] = useState(false)
   const [data, setData] = useState([])
   const [scroll, setScroll] = useState(0)
   const [direction, setDirection] = useState(true)
   const scrollSection = useRef<HTMLDivElement>()
   const name = useRef<string>()
-  
+
   useEffect(() => {
     if (!name.current) name.current = prompt('Enter your name: ')
     window.addEventListener('scroll', handleScroll)
     window.addEventListener('mousedown', handleMiddle)
     window.addEventListener('keydown', handleKeys)
-    window.scrollTo(0, 0)  
+    window.scrollTo(0, 0)
     return () => {
       window.removeEventListener('scroll', handleScroll)
       window.removeEventListener('mousedown', handleMiddle)
@@ -41,31 +44,31 @@ const Home: NextPage = () => {
   }, [])
 
   const getData = async () => {
-    const encrypted = encryptData(''+scroll, name); 
+    const crypted = encrypt(`${name.current}&${scroll}`, 'key')
     const req = await axios.post('https://scrollRank.aleksandrlitvin.repl.co', {
-        name: `${name.current}`,
-        scroll: encrypted || 0
+      name: `${name.current}`,
+      scroll: crypted || 0
     })
     const newData = await req.data;
     return setData([...newData]);
-    };
+  };
 
   const handleMiddle = (event) => {
     if (event.button === 1) event.preventDefault()
   }
 
-const handleKeys = (event) => {
+  const handleKeys = (event) => {
     const keysToDisable = ['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'PageUp', 'PageDown', 'Home', 'End', ' '];
     if (keysToDisable.includes(event.key)) event.preventDefault()
-}
+  }
 
   const handleScroll = (event) => {
-    
-    setScroll((prev)=>++prev)
-    if (window.pageYOffset > scrollSection.current.clientHeight/2) {
-      window.scrollTo(0, 0)  
+
+    setScroll((prev) => ++prev)
+    if (window.pageYOffset > scrollSection.current.clientHeight / 2) {
+      window.scrollTo(0, 0)
     }
-    }
+  }
 
   const handleClick = () => {
     event.preventDefault();
@@ -73,26 +76,26 @@ const handleKeys = (event) => {
     setIsModalOpen(true)
     setScroll(0)
 
-}
+  }
 
   const handleClose = () => {
     setIsModalOpen(false)
   }
 
-const nameCutter = (str, num) => {
+  const nameCutter = (str, num) => {
     if (str.length > num) {
-        return str.slice(0, num) + '...';
+      return str.slice(0, num) + '...';
     } else {
-        return str;
+      return str;
     }
-}
+  }
 
   return (
     <div className={styles.container}>
       <nav className={styles.nav}>
         <h1 className={styles.title}>{`Scroll rank: ${scroll}`}</h1>
-        <button 
-          className={joinClasses("btn", "showModal")} 
+        <button
+          className={joinClasses("btn", "showModal")}
           onClick={handleClick}>Save</button>
       </nav>
       <section ref={scrollSection} className={styles.scroll}
@@ -103,23 +106,23 @@ const nameCutter = (str, num) => {
         }>
       </section>
 
-      
+
       <section className={styles.mymodal}
-        
+
         style={{
           display: isModalOpen ? 'flex' : 'none',
         }}
-          
-        >
+
+      >
         <button className={styles.myclose} onClick={handleClose}>X</button>
         <div className={styles.modalСontainer}>
           <h2 className={styles.rank}>Top 10:</h2>
           <ol className={styles.rankList}>
-            {data.map((item,idx)=>{
-            if (item?.name) {
-              return <li key={idx}>{`${item.scroll} ${nameCutter(item.name, 20)}`}</li>
-            }
-               
+            {data.map((item, idx) => {
+              if (item?.name) {
+                return <li key={idx}>{`${item.scroll} ${nameCutter(item.name, 20)}`}</li>
+              }
+
             })}
           </ol>
         </div>
@@ -127,6 +130,5 @@ const nameCutter = (str, num) => {
     </div>
   )
 }
-
 
 export default Home
